c6228c580fb9f67e3aaa1f71ca40a7b3cde2a869
ani <aninur.h@gmail.com>
2022-06-08 14:47:04 +0700

    .

--- admin/home.php
+++ admin/home.php
@@ -2,7 +2,7 @@
 session_start();
 require_once("../config/koneksi_db.php");
 require_once("../config/config.php");
-//security_login();
+security_login();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -79,4 +79,3 @@ require_once("../config/config.php");
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
 	integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-< />
